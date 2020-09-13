1f3d3f6d2a2e474cd41d2d62e39dca9cc3f889e3
shuukeyare099 <koryaal6@gmail.com>
2020-09-13 02:04:00 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({path: './config.env'});
+const App = require('./App');
+
+process.on('uncaughtException', (err) => {
+	console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+	console.log(err.name, err.message);
+	process.exit(1);
+});
+
+const DB = process.env.DATABASE_LOCAL;
+console.log(DB);
+// const DB = process.env.DATABASE;
+mongoose
+	.connect(DB, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true,
+	})
+	.catch((err) => {
+		console.log(Error, err.message);
+	})
+	.then(console.log('DB successfully connected'));
+
+const port = process.env.PORT || 5000;
+const server = App.listen(port, () => {
+	console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+	console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+	console.log(err.name, err.message);
+	server.close(() => {
+		process.exit(1);
+	});
+});
+
+process.on('SIGTERM', () => {
+	console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+	server.close(() => {
+		console.log('💥 Process terminated!');
+	});
+});
